5901fd893ab0c64a6dc894141c47262deed15eb8
Saul Baizman <saul.baizman@massart.edu>
2025-06-15 12:53:57 -0400

    Refactored list assignments.
    [skip ci]

--- make-website-toc.py
+++ make-website-toc.py
@@ -56,8 +56,7 @@ def get_course_names(tsv):
         # skip header.
         next(course_names)
         for row in course_names:
-            course_number = row[0]
-            course_name = row[1]
+            course_number, course_name = row
             course_number_clean = course_number.replace('-', ' ')
             # manual fix for one intensive.
             course_number_clean = course_number_clean.replace('2035i', '2035')
@@ -76,11 +75,7 @@ def get_courses(tsv):
         # skip header.
         next(courses)
         for row in courses:
-            number = row[0]
-            url = row[1]
-            section = row[2]
-            season = row[3]
-            year = row[4]
+            number, url, section, season, year = row
             courses_list.append({
                 'number': number,
                 'url': url,
@@ -104,7 +99,8 @@ def main():
         current_semester = f'{course.get('season')} {course.get('year')}'
         course_name = course_names.get(course.get('number')).get('name')
         # do not use course.get('number'). it has a dash and an issue with a course exception.
-        course_number = course_names.get(course.get('number')).get('number').upper()
+        course_number = course_names.get(course.get('number')).get('number')
+        course_number_uppercase = course_number.upper()
 
         if current_semester != previous_semester:
             if not is_first_ul:
@@ -113,13 +109,13 @@ def main():
                 is_first_ul = False  # toggle the variable.
             body.append(f'<h3>{current_semester}</h3>')
             body.append('<ul>')
-        course_number_and_section = course_number
+        course_number_and_maybe_section = course_number_uppercase
         if course.get('section') != '':
             # prepend a "0" to the section number via f-string format.
-            course_number_and_section += f'-{course.get('section'):0>2}'
+            course_number_and_maybe_section += f'-{course.get('section'):0>2}'
         body.append('<li>' + format_link(
             url=course.get('url'),
-            target=f'{course_name} <span class="course_number">{course_number_and_section}</span>'
+            target=f'{course_name} <span class="course_number">{course_number_and_maybe_section}</span>'
         ) + '</li>')
 
         previous_semester = current_semester
@@ -127,9 +123,11 @@ def main():
     body.append('</ul>')
 
     # output the data.
-    print(get_header(title=page_title, stylesheet=css_stylesheet))
-    print("\n".join(body))
-    print(get_footer())
+    print(
+        get_header(title=page_title, stylesheet=css_stylesheet),
+        "\n".join(body),
+        get_footer()
+    )
 
 
 if __name__ == "__main__":
